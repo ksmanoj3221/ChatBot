2b771c9f7d7025901b17b6656fb57fbdd4d33885
Manoj Kumar <2023.manoj.sudalai@ves.ac.in>
2024-05-11 23:39:57 +0530

    Get All Users

--- backend/dist/controller/user-controller.js
+++ backend/dist/controller/user-controller.js
@@ -1,4 +1,11 @@
-export const getAllUsers = () => {
-    //get all users from DB
+import User from "../model/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controller.js.map
--- backend/src/controller/user-controller.ts
+++ backend/src/controller/user-controller.ts
@@ -1,3 +1,15 @@
-export const getAllUsers = () => {
-  //get all users from DB
+import { Request, Response, NextFunction } from "express";
+import User from "../model/User.js";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
 };
